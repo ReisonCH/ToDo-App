3c340689fabce82cb9c8d47f8cd3e69fc209c085
Reison Corazao <72511256+ReisonCH@users.noreply.github.com>
2024-10-03 02:20:39 +0000

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,6 +12,6 @@
                 <ToasContainer />
             </div>
         </div>
-        @vite('resources/js/app.js')
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </body>
 </html>
